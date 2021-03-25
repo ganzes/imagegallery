8d4daa47f044062a56f26ac05be729312fd0320f
ganzes <charczuk.arkadiusz@gmail.com>
2021-03-26 00:11:42 +0100

    added save files to local folder on disk

--- src/main/java/imagegalleryganzes/imagegallery/ImagegallerywaApplication.java
+++ src/main/java/imagegalleryganzes/imagegallery/ImagegallerywaApplication.java
@@ -1,13 +1,25 @@
 package imagegalleryganzes.imagegallery;
 
+import imagegalleryganzes.imagegallery.repositories.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class ImagegallerywaApplication {
+public class ImagegallerywaApplication implements CommandLineRunner {
+
+	@Resource
+	FilesStorageService storageService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImagegallerywaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... arg) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
 }
